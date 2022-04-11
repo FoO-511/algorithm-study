dd087f6e8babef9d6716a37c74d191c6fc4057d6
5thofnovmbr <5thofnovmbr@gmail.com>
2022-04-11 22:29:10 +0900

    Semi implement prim's algorithm

--- 4386_constellation/prim's_algorithm.js
+++ 4386_constellation/prim's_algorithm.js
@@ -1,3 +1,42 @@
+function Star(id, x, y) {
+  this.id = Number(id);
+  this.x = Number(x);
+  this.y = Number(y);
+}
+function Edge(start, end, weight) {
+  this.start = start;
+  this.end = end;
+  this.weight = weight;
+}
+
+// 입력
+let [n, ...arr] = require("fs")
+  .readFileSync("./test.txt")
+  .toString()
+  .replaceAll("\r", "")
+  .split("\n");
+
+n = Number(n);
+
+// 그래프에 입력.( 정점과 간선에 값 입력 )
+let stars = [];
+let edges = [];
+
+for (let i in arr) {
+  star_x_y = arr[i].split(" ");
+  stars[i] = new Star(i, star_x_y[0], star_x_y[1]);
+}
+
+for (let i = 0; i < n; i++) {
+  for (let j = i + 1; j < n; j++) {
+    const s1 = stars[i];
+    const s2 = stars[j];
+
+    const weight = ((s1.x - s2.x) ** 2 + (s1.y - s2.y) ** 2) ** 0.5;
+    edges.push(new Edge(s1.id, s2.id, weight));
+  }
+}
+
 // 무향 연결 그래프에서 최소신장트리(MST)를 찾는 알고리즘.
 
 // 프림 알고리즘의 과정
@@ -6,104 +45,43 @@
 // 3. 그 간선에 연결된 T에 포함되지 않은 노드를 T에 포함.
 // 4. 모든 노드가 포함될 때까지 2, 3 반복.
 
-// * 가중치가 있는 인접리스트 그래프 구현
-function GraphNode(end, weight) {
-  this.end = end;
-  this.weight = weight;
-}
-
-class Graph {
-  constructor() {
-    this.edges = {};
-    this.nodes = [];
-  }
-
-  addNode(v) {
-    this.nodes.push(v);
-  }
-
-  addEdge(v1, v2, weight) {
-    if (this.nodes.indexOf(v1) === -1 || this.nodes.indexOf(v2) === -1) {
-      console.log("no such vertex");
-    } else {
-      const newNode = new GraphNode(null, weight);
-      newNode.end = v2;
-      this.edges[v1] = newNode;
-
-      const newNode2 = new GraphNode(null, weight);
-      newNode2.end = v1;
-      this.edges[v2] = newNode2;
-    }
-  }
-}
-
-// 그래프 초기화
-let g = new Graph();
-g.addNode("A");
-g.addNode("B");
-g.addNode("C");
-g.addNode("D");
-g.addNode("E");
-g.addNode("F");
-g.addNode("G");
-
-g.addEdge("A", "C", 100);
-g.addEdge("A", "B", 3);
-g.addEdge("A", "D", 4);
-g.addEdge("C", "D", 3);
-g.addEdge("D", "E", 8);
-g.addEdge("E", "F", 10);
-g.addEdge("B", "G", 9);
-
 // 프림 알고리즘
-const MST = new Graph(); // 최소신장트리가 들어갈 그래프
-let visited = new Set(); // 방문한 노드 저장
 
 // 1. 임의의 정점을 선택해 T에 추가
-let s = g.nodes[0];
-visited.add(s);
-MST.addNode(s);
+let MST_nodes = new Set();
+MST_nodes.add(stars[0].id);
 
-// 2. T에 포함된 노드와 T에 포함되지 않은 노드들의 간선 중 가중치가 가장 작은 간선을 찾음.
-// T에 포함된 노드의 간선들을 담는 리스트 선언(우선순위 큐를 사용하는게 나음)
-const edgeList = [];
-g.edges[s].forEach((edge) => {
-  edgeList.push(edge);
-});
+// T에 포함된 노드의 간선들을 담는 리스트(우선순위 큐를 사용하는게 나음)
+let tmp_edges = [];
 
-const currentEdge = edgeList
-  .sort(function (a, b) {
-    if (a.weight > b.weight) return 1;
-    else return -1;
-  })
-  .pop();
+let ans = 0;
 
-// 3. 그 간선에 연결된 T에 포함되지 않은 노드를 T에 포함.
 // 4. 모든 노드가 포함될 때까지 2, 3 반복.
+while (MST_nodes.size < n && ans < 4) {
+  // 2. T에 포함된 노드와 T에 포함되지 않은 노드들의 간선 중 가중치가 가장 작은 간선을 찾음.
+  // T에 포함된 노드의 간선들을 가중치가 작은 순으로 정렬
+  tmp_edges = [];
+  for (let i of MST_nodes) {
+    tmp_edges = tmp_edges.concat(
+      edges.filter(
+        (v) =>
+          (v.start === i && !MST_nodes.has(v.end)) ||
+          (v.end === i && !MST_nodes.has(v.start))
+      )
+    );
 
-while (edgeList.length !== 0) {
-  while (edgeList.length !== 0 && visited.indexOf(currentEdge[1]) !== -1) {
-    currentEdge = edgeList.pop();
-  }
-
-  let nextNode = currentEdge[1];
-
-  if (visited.indexOf(nextNode) === -1) {
-    MST.addNode(nextNode);
-    MST.addEdge(currentEdge[0], nextNode, currentEdge[2]);
-
-    g.edges[nextNode].forEach((edge) => {
-      edgeList.push(edge);
+    tmp_edges.sort(function (a, b) {
+      if (a.weight > b.weight) return 1;
+      else return -1;
     });
-    currentEdge = edgeList
-      .sort(function (a, b) {
-        if (a.weight > b.weight) return 1;
-        else return -1;
-      })
-      .pop();
-
-    s = nextNode;
   }
+
+  const shortest_edge = tmp_edges[0];
+
+  // 3. 그 간선에 연결된 T에 포함되지 않은 노드를 T에 포함.
+  ans += shortest_edge.weight;
+  MST_nodes.add(shortest_edge.start);
+  MST_nodes.add(shortest_edge.end);
 }
 
-console.log(MST);
+console.log(ans);
--- 4386_constellation/solve.js
+++ 4386_constellation/solve.js
@@ -1,9 +1,13 @@
+// 그래프 클래스 없이 로직만 구현.
+// 가중치가 있는 무방향 그래프 구현 필요.
+// 그래프의 정점
 function Star(id, x, y) {
   this.id = id;
   this.x = x;
   this.y = y;
 }
 
+// 그래프의 간선(가중치 있음)
 function Edge(start, end, weight) {
   this.start = start;
   this.end = end;
@@ -21,15 +25,20 @@ let [n, ...arr] = fs
 
 n = Number(n);
 
+// 그래프 정점 리스트
 let stars = new Array(n).fill(-1);
 
+// 입력받은 값을 정점으로 만들어 정점 리스트에 넣음.
 for (let i in arr) {
   tmp_pos = arr[i].split(" ");
   stars[i] = new Star(Number(i), Number(tmp_pos[0]), Number(tmp_pos[1]));
 }
-// console.log(stars);
 
+// 간선 리스트.
 edgeList = [];
+
+// 간선의 입력은 없는 상태에서 각 점들간 거리를 계산해
+// (시작 정점, 끝 정점, 가중치를 가지는)간선 객체를 만들어 간선 리스트에 넣음.
 for (let i = 0; i < n; i++) {
   for (let j = i + 1; j < n; j++) {
     const weight =
@@ -38,24 +47,29 @@ for (let i = 0; i < n; i++) {
   }
 }
 
-// console.log(edgeList);
+// 크러스컬 알고리즘 진행 준비
 
+// 간선리스트의 가중치 오름차순 정렬.
 edgeList.sort(function (a, b) {
   if (a.weight > b.weight) return 1;
   else return -1;
 });
 
-// console.log(edgeList);
+// 사이클을 만드는지 아닌지 판단하기 Union-find 기법을 이용.
 
+// 각자 스스로를 가리키는 배열 선언. parent[i] = i 상태.
 let parent = new Array(n);
 for (let i = 0; i < n; i++) parent[i] = i;
 
-let ans = 0;
-
+// find를 통해 parent 배열에서 어떤 인덱스의 최상위 부모를 찾음.
+// parent[i]가 i가 아니라 k일 경우 parent[k]를 탐색.
 function find(x) {
   if (x == parent[x]) return x;
   return (parent[x] = find(parent[x]));
 }
+
+// 초기에 각 인덱스가 각 배열요소의 값
+// union을 통해 parent[x]에 x가 아닌 새로운 값을 부여.
 function union(x, y) {
   x = find(x);
   y = find(y);
@@ -64,10 +78,19 @@ function union(x, y) {
   }
 }
 
+// 정답값. 최소신장트리의 가중치가 담김.
+let ans = 0;
+
+// 크러스컬 알고리즘 진행.
 for (let i = 0; i < edgeList.length; i++) {
+  // edge리스트의 간선이 처음부터 차례로 담김.(= 가장 작은 값부터 차례로)
   const edge = edgeList[i];
 
+  // 해당 간선의 정점이 같은 부모를 가지는지 검사(= 사이클을 이루는지 검사)
   if (find(edge.start) !== find(edge.end)) {
+    // 사이클을 이루지 않으면 리턴값과 parent 배열에 추가
+    // 최소신장트리를 위해 새로운 그래프를 만들지 않고 사이클 검사를 위해 정점만 parent배열에 추가됨.
+    // 어차피 간선이 짧은 순서대로 진행되기 때문에(간선 선택에 영향을 미치는 또다른 변수가 없기 때문에) 간선을 저장할 필요 없음.
     ans += edge.weight;
     union(edge.start, edge.end);
   }
